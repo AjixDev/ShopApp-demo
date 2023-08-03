06507dfdfbabd4e0b5ebf96fa03582035e00b93b
Roy Aji <ajixdev@gmail.com>
2023-08-03 19:05:56 +0300

    Product Screen Added - product view, route, add to cart button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,28 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap";
-import logo from "../assets/logo-50.png";
+import {Nav, Navbar, Container} from 'react-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
+import logo from '../assets/logo-50.png';
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt="ShopApp logo" className="me-2" />
+              <img src={logo} alt='ShopApp logo' className='me-2' />
               ShopApp
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic.navbar.nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic.navbar.nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaUser /> Sign In
                 </Nav.Link>
